dffe02c75da8fd7c45225e1d8bed7f922e1f4774
Nikita Novikov <gambitrus38@yandex.ru>
2021-10-20 12:59:44 +0300

    feat: сверстать страницу, задать стили, прописать логику JS

--- README.md
+++ README.md
@@ -1,17 +1,11 @@
 # Проект: Место
 
-### Обзор
+## Данный проект является проектной работой курса ЯПрактикум "Базовый JavaScript и работа с браузером."
 
-* Figma
-* Картинки
+### Цель проекта изучить и закрепить на практике основы работы с DOM.
 
-**Figma**
-
-* [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
-
-**Картинки**
-
-Доставать картинки предстоит из Фигмы. Это расхожая практика, поэтому полезно потренироваться.
-Не забудьте [оптимизировать картинки](https://tinypng.com/), чтобы ваш сайт загружался быстрее.
-
-Удачи!
+### Закрепление навыков
+* Работа с макетом в Figma
+* <code><img src="https://user-images.githubusercontent.com/56598375/123514858-3d309f80-d695-11eb-97b4-6a7a1ad2a920.png" alt="html-5" width="18px" height="18px"></code> HTML верстка, работа с таблицами стилей <code><img src="https://user-images.githubusercontent.com/56598375/123514865-428dea00-d695-11eb-9808-a5bcd93bbfb5.png" alt="html-5" width="18px" height="18px"></code>CSS по [BEM methodology](https://ru.bem.info/methodology/)
+* Работа с DOM деревом с помощью JavaScript
+* JavaScript (функции, слушатели и обработчики событий)
---
+++ scripts/index.js
@@ -0,0 +1,43 @@
+const popupElement = document.querySelector('.popup');
+const popupOpenButtonElement = document.querySelector('.profile__btn-edit');
+const popupCloseButtonElement = popupElement.querySelector('.popup__btn-close');
+const profileInfo = document.querySelector('.profile__info');
+const popupSaveInfo = popupElement.querySelector('.popup__btn-save');
+
+const closePopupByClickOverlay = function (event) {
+  if (event.target === event.currentTarget) {
+    closePopup();
+  }
+};
+
+const openPopup = function () {
+  popupElement.querySelector('#newName').value = profileInfo.querySelector('.profile__name').textContent;
+  popupElement.querySelector('#newActivity').value = profileInfo.querySelector('.profile__activity').textContent;
+  popupElement.classList.add('popup_is-opened');
+};
+
+const closePopup = function () {
+  popupElement.classList.remove('popup_is-opened');
+};
+
+const editProfile = function () {
+  event.preventDefault();
+  profileInfo.querySelector('.profile__name').textContent = popupElement.querySelector('#newName').value;
+  profileInfo.querySelector('.profile__activity').textContent = popupElement.querySelector('#newActivity').value;
+
+  // let oldProfileName = profileInfo.querySelector('.profile__name');
+  // let oldProfileActivity = profileInfo.querySelector('.profile__activity');
+  // let NewProfileName = popupElement.querySelector('#newName');
+  // let NewProfileActivity = popupElement.querySelector('#newActivity');
+
+  // oldProfileName.textContent = NewProfileName.value;
+  // oldProfileActivity.textContent = NewProfileActivity.value;
+
+  closePopup();
+}
+
+
+popupOpenButtonElement.addEventListener ('click', openPopup);
+popupCloseButtonElement.addEventListener ('click', closePopup);
+popupElement.addEventListener ('click', closePopupByClickOverlay);
+popupSaveInfo.addEventListener ('click', editProfile);
