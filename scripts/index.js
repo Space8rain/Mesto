3bf8eae2a20e2d96d3b8334a59ebe3d0fad351f0
Nikita Novikov <gambitrus38@yandex.ru>
2021-10-24 14:59:03 +0300

    refactor: исправление после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,15 @@ const popupOpenButtonElement = document.querySelector('.profile__btn-edit');
 const popupCloseButtonElement = popupElement.querySelector('.popup__btn-close');
 const profileInfo = document.querySelector('.profile__info');
 const submitProfile = popupElement.querySelector('.form')
-let oldProfileName = profileInfo.querySelector('.profile__name');
-let oldProfileActivity = profileInfo.querySelector('.profile__activity');
-let NewProfileName = popupElement.querySelector('#newName');
-let NewProfileActivity = popupElement.querySelector('#newActivity');
+const oldProfileName = profileInfo.querySelector('.profile__name');
+const oldProfileActivity = profileInfo.querySelector('.profile__activity');
+const newProfileName = popupElement.querySelector('#newName');
+const newProfileActivity = popupElement.querySelector('#newActivity');
 
 
 const openPopup = function () {
-  NewProfileName.value = oldProfileName.textContent
-  NewProfileActivity.value = oldProfileActivity.textContent
+  newProfileName.value = oldProfileName.textContent
+  newProfileActivity.value = oldProfileActivity.textContent
   popupElement.classList.add('popup_is-opened');
 };
 
@@ -21,8 +21,8 @@ const closePopup = function () {
 
 const editProfile = function (event) {
   event.preventDefault();
-  oldProfileName.textContent = NewProfileName.value;
-  oldProfileActivity.textContent = NewProfileActivity.value;
+  oldProfileName.textContent = newProfileName.value;
+  oldProfileActivity.textContent = newProfileActivity.value;
 
   closePopup();
 }
